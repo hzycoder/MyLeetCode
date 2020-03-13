1b2e3b2817d30b2a8ce3e1411ff1638c95bdd046
park.huang <park.huang@zkteco.com>
2020-03-13 17:25:17 +0800

    673. 最长递增子序列的个数 动态规划 难度太大 未通过

---
+++ src/cn/zy/findNumberOfLIS/Solution.java
@@ -0,0 +1,33 @@
+package cn.zy.findNumberOfLIS;
+
+/**
+ * 673. 最长递增子序列的个数
+ * https://leetcode-cn.com/problems/number-of-longest-increasing-subsequence/
+ * create by park.huang 2020/03/12
+ * 未通过
+ **/
+public class Solution {
+    public int findNumberOfLIS(int[] nums) {
+        int length = 1;
+        int temp = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > temp) {
+                temp = nums[i];
+                length++;
+            }
+        }
+        for (int i = 1; i < nums.length; i++) {
+
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+//        int[] nums = new int[]{1, 3, 4, 6, 3};
+        int[] nums = new int[]{10, 12, 13, 2, 3, 4, 5, 6};
+
+        int numberOfLIS = new Solution().findNumberOfLIS(nums);
+        System.out.println(numberOfLIS);
+        ;
+    }
+}
